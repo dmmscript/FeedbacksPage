60a5e90e5ac3137cddb694e8f6f5ddf71a7006c9
Danielly Moro <daniellymoro12@gmail.com>
2025-04-05 15:33:17 -0300

    feat: function get

--- api/sendFeedback.js
+++ api/sendFeedback.js
@@ -1,40 +1,43 @@
-function sanitizeInput(input) {
-    return input.replace(/[' ";]/g, "");
-}
-
 export default async function handler(req, res) {
-    if (req.method !== "POST") {
-        return res.status(405).json({ error: "Método não permitido" });
-    }
-    
-    try {
+    const GOOGLE_SCRIPT_URL = process.env.GOOGLE_SCRIPT_URL;
+  
+    if (req.method === "POST") {
+      try {
         const { nome, avaliacao, comentario } = req.body;
-        
-        const sanitizedNome = sanitizeInput(nome);
-        const sanitizedAvaliacao = sanitizeInput(avaliacao);
-        const sanitizedComentario = sanitizeInput(comentario);
-
+  
         const ip = req.headers["x-forwarded-for"]?.split(",")[0] || req.socket.remoteAddress;
-        
-        const GOOGLE_SCRIPT_URL = process.env.GOOGLE_SCRIPT_URL;
-
+  
         const payload = {
-            nome: sanitizedNome,
-            avaliacao: sanitizedAvaliacao,
-            comentario: sanitizedComentario,
-            ip
+          nome,
+          avaliacao,
+          comentario,
+          ip
         };
-
+  
         const response = await fetch(GOOGLE_SCRIPT_URL, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(payload)
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(payload)
         });
-
+  
         const data = await response.json();
         res.status(response.status).json({ message: data });
-        
-    } catch (error) {
+  
+      } catch (error) {
         res.status(500).json({ error: "Erro ao enviar feedback" });
+      }
+  
+    } else if (req.method === "GET") {
+      try {
+        const response = await fetch(GOOGLE_SCRIPT_URL);
+        const data = await response.json();
+        res.status(200).json(data);
+      } catch (error) {
+        res.status(500).json({ error: "Erro ao buscar feedbacks" });
+      }
+  
+    } else {
+      res.status(405).json({ error: "Método não permitido" });
     }
-}
+  }
+  
